4dfe30de4e6dfa95021d9abdb7e89516191aa8a2
SHANIB C K <ckshanib8@gmail.com>
2023-12-31 10:37:37 +0530

    News page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
+import News from "./components/News";
 
 function App() {
   return (
     <>
       <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
         <Navbar />
+        <News />
       </div>
     </>
   );
