aaadffcfa99b9a0d5c33fe8b9d382bd2570c7270
SHANIB C K <ckshanib8@gmail.com>
2023-12-31 09:49:32 +0530

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-      <Navbar />
+      <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+        <Navbar />
+      </div>
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,57 @@
+// This is Navbar page
+
+// This is importing section
 import React from "react";
+import { FaCartShopping } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
     <>
-      <div>Navbar</div>
+      <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200">
+        <div className="container py-3 sm:py-0">
+          <div className="flex justify-between items-cennter">
+            <div className="py-2">
+              <a
+                href="#"
+                className="flex items-center gap-2 text-2xl md:3xl font-bold"
+              >
+                <h1>News App</h1>
+              </a>
+            </div>
+            <div className="flex items-center gap-4">
+              <div>
+                {/* This is DarkMode page call */}
+              </div>
+              <ul className="hidden sm:flex gap-4">
+                <li>
+                  <a
+                    href="#"
+                    className="inline-block py-4 px-4 hover:text-primary"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="inline-block py-4 px-4 hover:text-primary"
+                  >
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="inline-block py-4 px-4 hover:text-primary"
+                  >
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
