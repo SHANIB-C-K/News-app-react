55b8488bf16b71815262bb21147f4cdcaf518ce4
SHANIB C K <ckshanib8@gmail.com>
2023-12-31 15:42:35 +0530

    Navbar design changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,40 +21,60 @@ const Navbar = ({ active, setActive, setCategory }) => {
   }
   return (
     <>
-      <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200">
-        <div className="container py-3 sm:py-0">
-          <div className="flex justify-between items-cennter">
-            <div className="py-2">
-              <a
-                href="/"
-                className="flex items-center gap-2 text-2xl md:3xl font-bold"
-              >
-                <h1>News App</h1>
-              </a>
-            </div>
-            <div className="flex items-center gap-4">
-              <div>
-                <DarkMode />
-                {/* This is DarkMode page call */}
-              </div>
-              <ul className=" lg:block flex space-x-8">
-                {
-                  /* Map through the array of objects and create a list item for each object.*/
-                  links.map((link) => (
-                    <li
-                      key={link.id}
-                      className="inline-block py-4 px-4 hover:text-primary"
-                      onClick={() => OnClick(link.id, link.value)}
-                    >
-                      {link.name}
-                    </li>
-                  ))
-                }
-              </ul>
-            </div>
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a
+            href="https://flowbite.com/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-8"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Flowbite
+            </span>
+          </a>
+          <button
+            data-collapse-toggle="navbar-default"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-default"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              {links.map((link) => (
+                <li
+                  key={link.id}
+                  className="block py-2 px-3 lg:text-black hover:text-primary bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                  onClick={() => OnClick(link.id, link.value)}
+                >
+                  {link.name}
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
-      </div>
+      </nav>
     </>
   );
 };
