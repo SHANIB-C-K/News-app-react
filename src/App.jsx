d0eedf05f3bfff94d1eafe7557dd590ae136f8e6
SHANIB C K <ckshanib8@gmail.com>
2023-12-31 12:12:28 +0530

    catagory usestate created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import News from "./components/News";
 import NewsItem from "./components/NewsItem";
 
 function App() {
+  const [catagory, setCatagory] = useState("general");
   return (
     <>
       <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
