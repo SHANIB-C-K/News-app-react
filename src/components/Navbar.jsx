8059fa49e0f74ca7ea82f97def88bdf86c7ae2a6
SHANIB C K <ckshanib8@gmail.com>
2024-01-01 09:22:51 +0530

    menu button added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@
 // This is importing section
 import React from "react";
 import DarkMode from "./DarkMode";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = ({ setActive, setCategory }) => {
   const links = [
@@ -32,47 +33,36 @@ const Navbar = ({ setActive, setCategory }) => {
             </span>
           </a>
           <div className="flex justify-between items-center">
-          <div className="pr-4">
-            <DarkMode />
-            {/* This is DarkMode page call */}
-          </div>
-          <button
-            data-collapse-toggle="navbar-default"
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-default"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
+            <div className="pr-4">
+              <DarkMode />
+              {/* This is DarkMode page call */}
+            </div>
+            <button
+              data-collapse-toggle="navbar-default"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-default"
+              aria-expanded="false"
             >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-          <div className="hidden w-full lg:block lg:w-auto" id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 lg:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 lg:flex-row lg:space-x-8 rtl:space-x-reverse lg:mt-0 lg:border-0 lg:bg-white dark:bg-gray-800 lg:dark:bg-gray-900 dark:border-gray-700">
-              {links.map((link) => (
-                <li
-                  key={link.id}
-                  className="block py-2 px-3 lg:text-black hover:text-primary bg-blue-700 rounded lg:bg-transparent lg:text-blue-700 lg:p-0 dark:text-white lg:dark:text-blue-500"
-                  onClick={() => OnClick(link.id, link.value)}
-                >
-                  {link.name}
-                </li>
-              ))}
-            </ul>
-          </div>
+              <span className="sr-only">Open main menu</span>
+              <GiHamburgerMenu className="w-20 h-20 lg:hidden" />
+            </button>
+            <div
+              className="hidden w-full lg:block lg:w-auto"
+              id="navbar-default"
+            >
+              <ul className="font-medium flex flex-col p-4 lg:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 lg:flex-row lg:space-x-8 rtl:space-x-reverse lg:mt-0 lg:border-0 lg:bg-white dark:bg-gray-800 lg:dark:bg-gray-900 dark:border-gray-700">
+                {links.map((link) => (
+                  <li
+                    key={link.id}
+                    className="block py-2 px-3 hover:text-primary bg-blue-700 rounded lg:bg-transparent lg:text-blue-700 lg:p-0 dark:text-white lg:dark:text-blue-500"
+                    onClick={() => OnClick(link.id, link.value)}
+                  >
+                    {link.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
