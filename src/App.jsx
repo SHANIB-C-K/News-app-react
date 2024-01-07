603c721fd926e8c5af3e59c8b6d1bf96f8906545
SHANIB C K <134039228+SHANIB-C-K@users.noreply.github.com>
2024-01-07 23:43:31 +0530

    Comments added in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+//Importing section 
+
 import { useState } from "react";
 import Navbar from "./components/Navbar";
 import News from "./components/News";
 import NewsItem from "./components/NewsItem";
 
 function App() {
+  //usestates
   const [category, setCategory] = useState("general");
   const [isLoading, setIsLoading] = useState(false);
 
   return (
     <>
+      {/* This is Navbar page */}
       <Navbar setCategory={setCategory} />
+      {/* This is News page */ }
       <News category={category} setIsLoading={setIsLoading} />
+      {/* This is NewsItem page */}
       <NewsItem isLoading={isLoading} />
     </>
   );
